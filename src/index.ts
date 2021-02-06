372dd0ae7eb23679328b49d0b6545fdb5e49c92c
Nicolai Gram <nicolaigram@gmail.com>
2021-02-06 14:12:27 +0100

    Install socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hi!")
+import { Server } from "http";
+const server: Server = require('http').createServer();
+const io: SocketIO.Server = require('socket.io')(server);
+
+io.on('connection', client => {
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
+
+server.listen(3000);
