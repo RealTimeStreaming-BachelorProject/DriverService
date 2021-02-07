49c2e71a1ece2bea01b944f19f8e6df5c5307f16
Nicolai Gram <nicolaigram@gmail.com>
2021-02-07 12:55:40 +0100

    Setup simple redis listening on key

--- src/listeners/positionListener/index.ts
+++ src/listeners/positionListener/index.ts
@@ -1,9 +1,19 @@
 import redisClient from '../../persistence'
+import { onKeyChange } from '../util/redisSubscribe';
 
 export const setup = (client: SocketIO.Socket) => {
-  client.on("set-position", (position) => {
-    redisClient.set(`driver-${client.id}`, position)
+  
+  const driverId = 1;
+
+  onKeyChange(`driver-${driverId}`, (key, value) => {
+    console.log(`Key ${key} was changed`);
+    client.emit("last-position", value);
+  })
+
+  client.on("set-position", ({coordinates, driverId}) => {
+    redisClient.set(`driver-${driverId}`, coordinates)
   });
+
   client.on("get-position", (driverId) => {
     const lastPosition = redisClient.get(`driver-${driverId}`)
     client.emit("last-position", lastPosition);
