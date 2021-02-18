c499140c40019fad2ff921bfd8b5e3a35ae2ef58
omvk97 <olvan18@student.sdu.dk>
2021-02-18 11:36:45 +0100

    proper jwt handling

--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -5,28 +5,27 @@ import { jwtDecoded } from "../helpers/jwt.helpers";
 import { IDecodedJWT } from "../interfaces/authentication.interfaces";
 import { DRIVER_NAMESPACE, USER_NAMESPACE } from "../routes";
 import { driverListeners, userListeners } from "./listeners";
+import { authorize } from "socketio-jwt";
+import { AUTHENTICATED } from "../events";
+const JWT_KEY: string = process.env["JWT_KEY"] ?? "developmentjwtkey";
 
 export const configureIOServer = (io: Server) => {
-  // note: middleware is only executed once per connection
-  // only drivers need to authorize
-  io.of(DRIVER_NAMESPACE).use((socket: Socket, next: any) => {
-    try {
-      const token: string = socket.handshake.query.token as string;
-      const decodedjwt: IDecodedJWT = jwtDecoded(token);
-      connections.set(socket.id, decodedjwt.driverID);
-      next();
-    } catch (error) {
-      // All middlewares will be stopped if next is called with an error
-      next(error);
-    }
-  });
-
-  io.of(DRIVER_NAMESPACE).on("connection", (socket: Socket) => {
-    logger.info(`Driver ${socket.id} connected`);
-    for (const listener of driverListeners) {
-      listener(socket);
-    }
-  });
+  io.of(DRIVER_NAMESPACE)
+    .on(
+      "connection",
+      authorize({
+        secret: JWT_KEY,
+        timeout: 15000,
+      })
+    )
+    .on(AUTHENTICATED, (socket: any) => {
+      logger.info(`Driver ${socket.id} connected and authenticated`);
+      connections.set(socket.id, socket.decoded_token.driverID);
+      
+      for (const listener of driverListeners) {
+        listener(socket);
+      }
+    });
 
   // TODO: Figure out what to do upon the event of driver disconnecting. Clean up packages in redis? Clean up all their subsribers? Something something
   io.of(USER_NAMESPACE).on("connection", (socket: Socket) => {
