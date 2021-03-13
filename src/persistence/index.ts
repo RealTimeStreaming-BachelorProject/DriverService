91c38defb50ccc453f07361bf2693a98d7e37698
omvk97 <olvan18@student.sdu.dk>
2021-03-13 12:40:24 +0100

    redis cluster removal

--- src/persistence/index.ts
+++ src/persistence/index.ts
@@ -4,7 +4,6 @@ import logger from "../helpers/logger";
 let redisNodes: any;
 
 if (process.env.REDIS_CLUSTER_NODES) {
-  console.log("What")
   redisNodes = JSON.parse(process.env.REDIS_CLUSTER_NODES);
 } else {
   redisNodes = [
@@ -16,30 +15,12 @@ if (process.env.REDIS_CLUSTER_NODES) {
       port: 6380,
       host: "127.0.0.1",
     },
-    {
-      port: 6381,
-      host: "127.0.0.1",
-    },
-    {
-      port: 6382,
-      host: "127.0.0.1",
-    },
-    {
-      port: 6383,
-      host: "127.0.0.1",
-    },
-    {
-      port: 6384,
-      host: "127.0.0.1",
-    },
   ];
 }
+
 console.log(redisNodes)
 export const cluster = new Redis.Cluster(redisNodes, {
   scaleReads: "slave", // sending write queries to master and read queries to slave
-  redisOptions: {
-    password: "bitnami"
-  }
 });
 
 cluster.on("error", function (error) {
